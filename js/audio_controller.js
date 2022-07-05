e63e61d4f24a1620c95dda9602e8921aeae7bf9c
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-07-04 22:42:59 -0400

    update

--- js/audio_controller.js
+++ js/audio_controller.js
@@ -46,8 +46,8 @@ audio_controller.startNote = function(frequency, volume_percent, harmonicsVolume
 		gain.gain.setValueAtTime(0, this.ctx.currentTime);
 		gain.gain.linearRampToValueAtTime(1, this.ctx.currentTime + attack);
 
-		var decay_time = 0.01;
-		var decay_amount_percent = 10;
+		var decay_time = 0.1;
+		var decay_amount_percent = 30;
 
 		gain.gain.linearRampToValueAtTime( 1*((100-decay_amount_percent)/100), this.ctx.currentTime + attack + decay_time);
 		osc.start();
